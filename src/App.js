4b95c3fa87a6c5c7b11c95c669e942f19e0af7cc
Leonardo <89220518+leodiman182@users.noreply.github.com>
2023-03-23 17:12:16 -0300

    add efeito estático no header

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Header from "./components/Header";
+import Header from "./components/Header/index";
 import AlertComponent from "./components/AlertComponent";
 import { Stack } from "@mui/material";
 import TimerButton from "./components/TimerButton";
@@ -13,9 +13,14 @@ function App() {
     <Header />
     <AlertComponent />
     <main className="main bg-slate-100 flex flex-col items-center justify-center">
-      <Stack direction='column' spacing={2} alignItems='center'>
-        <ResetAlert />    
-        <TimerButton />
+      <h2 className="font-bold text-primary text-center text-[20px] md:text-[24px] px-[20px] md:px-0 mb-[6px]">
+        Tente somar, subtrair, ligar ou pausar o cronômetro.
+      </h2>
+      <Stack direction='column' spacing={1} alignItems='center'>
+        <Stack direction='row' spacing={2}>
+          <TimerButton />
+          <ResetAlert />
+        </Stack>
         <Timer />
         <InteractionButtons />
       </Stack>
